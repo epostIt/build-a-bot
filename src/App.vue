7d28a2c1197b07e3dbb51134100612a146628982
Elisabeth Post <epost@uccs.edu>
2020-10-13 15:25:21 -0600

    first push from desktop

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="./assets/build-a-bot-logo.png">
+            Build-a-bot
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <HomePage />
+      <RobotBuilder />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HomePage from './components/HomePage.vue';
+import RobotBuilder from './components/RobotBuilder.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    HomePage,
+    RobotBuilder,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
